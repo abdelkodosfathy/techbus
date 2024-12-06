fb9e8bd345881d4d5db5906ed0048eb491431b1c
abdelkodosfathy <abdelkodosfathy@gmail.com>
2024-12-06 04:27:37 +0200

    added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
-
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,4 +11,6 @@ export default defineConfig({
       'assets': path.resolve(__dirname, 'src/assets'),
     },
   },
+  // Make sure the base path is set correctly for GitHub Pages
+  base: '/techbus',  // Include trailing slash
 })
